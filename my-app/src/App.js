40016a0dc2821f0a9f7afb4933ab03286bb39716
Xtramemium <93661341+Xtramemium@users.noreply.github.com>
2023-05-10 00:29:46 +0300

    gitiignore and appjs updated

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -25,44 +25,45 @@ const date = new Date().toDateString().toLocaleString()
 //         </div>  // конец
 //     );
 // }
+export const App = () => {
+    return (
+        // декларативный подход начало
+        React.createElement(
+            "div",
+            {
+                className: "App"
+            },
+            React.createElement(
+                "header",
+                {
+                    className: "App-header"
+                },
+                React.createElement("img", {
+                    src: logo,
+                    className: "App-logo",
+                    alt: "logo"
+                }), React.createElement(
+                    "p",
+                    null,
+                    "Edit ",
+                    React.createElement("code", null, "src/App.js"),
+                    "we can handle it."
+                ),
+                React.createElement(
+                    "a",
+                    {
+                        className: "App-link",
+                        href: "https://reactjs.org",
+                        target: "_blank",
+                        rel: "noopener noreferrer"
+                    },
+                    "Learn React"
+                ),
+                React.createElement("span", null, date)
+            )
+        ) // конец
+    );
+};
 
-export const createMainPage = () => {
-    const div = document.createElement('div')
-    div.className = 'App'
-
-    const header = document.createElement('header')
-    header.className = "App-header"
-
-    const img = document.createElement('img')
-    img.src = logo
-    img.className = 'App-logo'
-    img.alt = 'logo'
-
-    const p = document.createElement('p')
-    p.textContent = `Edit как нибудь разберемся.`
-
-    const code = document.createElement('code')
-    code.textContent = 'src/App.js'
-
-    const a = document.createElement('a')
-    a.className = 'App-link'
-    a.href = 'https://reactjs.org'
-    a.target = '_blank'
-    a.rel = 'noopener noreferrer'
-    a.textContent = 'Learn React'
-
-    const span = document.createElement('span')
-    span.textContent = date
-
-
-    div.append(header)
-    header.append(img, p, a, span)
-    p.insertAdjacentElement('afterbegin', code)
-
-    return div
-}
-
-const mainPage = createMainPage()
-document.body.append(mainPage)
 
 

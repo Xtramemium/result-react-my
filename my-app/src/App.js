ed331692b6c6f4b29a76dbfb32a418e322aeb9ee
Xtramemium <93661341+Xtramemium@users.noreply.github.com>
2023-05-09 14:16:30 +0300

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,29 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 const date = new Date().toDateString().toLocaleString()
 
-export const App = () => {
-    return (     // декларативный подход начало
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> как нибудь разберемся .
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-                <span>
-                    {date}
-                </span>
-            </header>
-        </div>  // конец
-    );
-}
+// export const App = () => {
+//     return (     // декларативный подход начало
+//         <div className="App">
+//             <header className="App-header">
+//                 <img src={logo} className="App-logo" alt="logo"/>
+//                 <p>
+//                     Edit <code>src/App.js</code> как нибудь разберемся .
+//                 </p>
+//                 <a
+//                     className="App-link"
+//                     href="https://reactjs.org"
+//                     target="_blank"
+//                     rel="noopener noreferrer"
+//                 >
+//                     Learn React
+//                 </a>
+//                 <span>
+//                     {date}
+//                 </span>
+//             </header>
+//         </div>  // конец
+//     );
+// }
 
 export const createMainPage = () => {
     const div = document.createElement('div')
@@ -39,7 +39,10 @@ export const createMainPage = () => {
     img.alt = 'logo'
 
     const p = document.createElement('p')
-    p.textContent = `Edit ${<code>src/App.js</code>} как нибудь разберемся.`
+    p.textContent = `Edit как нибудь разберемся.`
+
+    const code = document.createElement('code')
+    code.textContent = 'src/App.js'
 
     const a = document.createElement('a')
     a.className = 'App-link'
@@ -48,9 +51,13 @@ export const createMainPage = () => {
     a.rel = 'noopener noreferrer'
     a.textContent = 'Learn React'
 
+    const span = document.createElement('span')
+    span.textContent = date
+
 
     div.append(header)
-    header.append(img, p, a)
+    header.append(img, p, a, span)
+    p.insertAdjacentElement('afterbegin', code)
 
     return div
 }
